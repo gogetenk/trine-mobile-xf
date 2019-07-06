7c523ff1e69b88ced858106fd4b51ef76aa131fc
Yannis <gogetenk@hotmail.com>
2019-07-06 21:45:08 +0200

    sqdqsdsq

--- src/native/Android/appcenter-post-build.sh
+++ src/native/Android/appcenter-post-build.sh
@@ -1,40 +1,48 @@
 #!/usr/bin/env bash
 
-# Post Build Script ANDROID
+    echo "Post Build Script Started"
 
-set -e # Exit immediately if a command exits with a non-zero status (failure)
+    SolutionFile=`find "$APPCENTER_SOURCE_DIRECTORY" -name Trine.Mobile.sln`
+    SolutionFileFolder=`dirname $SolutionFile`
 
-echo "**************************************************************************************************"
-echo "Post Build Script"
-echo "**************************************************************************************************"
+    UITestProject=`find "$APPCENTER_SOURCE_DIRECTORY" -name Modules.Authentication.UITests.csproj`
 
-echo "list files in APPCENTER_SOURCE_DIRECTORY"
+    echo SolutionFile: $SolutionFile
+    echo SolutionFileFolder: $SolutionFileFolder
+    echo UITestProject: $UITestProject
 
-##################################################
-# Start UI Tests
-##################################################
+    chmod -R 777 $SolutionFileFolder
 
-# variables
-appCenterLoginApiToken=$AppCenterLoginForAutomatedUITests # this comes from the build environment variables
-appName="Trine-App/Trine-Android"
-deviceSetName="b82043e1"
-testSeriesName="ui-tests"
-APKFile=`find "$APPCENTER_SOURCE_DIRECTORY" -name *.apk | head -1`
-UITestDLL=`find "$APPCENTER_SOURCE_DIRECTORY" -name "Modules.Authentication.UITests.dll" | grep bin`
+    msbuild "$UITestProject" /property:Configuration=$APPCENTER_XAMARIN_CONFIGURATION
 
+    UITestDLL=`find "$APPCENTER_SOURCE_DIRECTORY" -name "Modules.Authentication.UITests.dll" | grep bin`
+    echo UITestDLL: $UITestDLL
 
-echo ""
-echo "Start Xamarin.UITest run"
-echo "   App Name: $appName"
-echo " Device Set: $deviceSetName"
-echo "Test Series: $testSeriesName"
-echo ""
+    UITestBuildDir=`dirname $UITestDLL`
+    echo UITestBuildDir: $UITestBuildDir
 
-echo "> Run UI test command"
-# Note: must put a space after each parameter/value pair 
-appcenter test run uitest --app $appName --devices $deviceSetName --app-path $APKFile --test-series $testSeriesName --locale "fr_FR" --build-dir $UITestDLL --uitest-tools-dir /Users/vsts/.nuget/packages/xamarin.uitest/3.0.0/tools --token $appCenterLoginApiToken 
+    UITestVersionNumber=`grep '[0-9]' $UITestProject | grep Xamarin.UITest|grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,10\}\-'dev`
+    echo UITestPrereleaseVersionNumber: $UITestVersionNumber
 
-echo ""
-echo "**************************************************************************************************"
-echo "Post Build Script complete"
-echo "**************************************************************************************************"
+    UITestVersionNumberSize=${#UITestVersionNumber} 
+    echo UITestVersionNumberSize: $UITestVersionNumberSize
+
+    if [ $UITestVersionNumberSize == 0 ]
+    then
+        UITestVersionNumber=`grep '[0-9]' $UITestProject | grep Xamarin.UITest|grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'`
+        echo UITestVersionNumber: $UITestVersionNumber
+    fi
+
+    TestCloudExe=`find ~/.nuget | grep test-cloud.exe | grep $UITestVersionNumber | head -1`
+    echo TestCloudExe: $TestCloudExe
+
+    TestCloudExeDirectory=`dirname $TestCloudExe`
+    echo TestCloudExeDirectory: $TestCloudExeDirectory
+
+    APKFile=`find "$APPCENTER_SOURCE_DIRECTORY" -name *.apk | head -1`
+
+    npm install -g appcenter-cli
+
+    appcenter login --token token
+
+    appcenter test run uitest --app "Trine-App/Trine-Android" --devices "b82043e1" --app-path $APKFile --test-series "ui-tests" --locale "fr_FR" --build-dir $UITestBuildDir --uitest-tools-dir $TestCloudExeDirectory --async
