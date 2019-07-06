f030b53eee5ce2f7c9194b12c6d35973102c476b
Yannis <gogetenk@hotmail.com>
2019-07-06 15:56:52 +0200

    added post build script file for UI Tests

---
+++ Trine.Mobile/appcenter-post-build.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Post Build Script
+
+set -e # Exit immediately if a command exits with a non-zero status (failure)
+
+echo "**************************************************************************************************"
+echo "Post Build Script"
+echo "**************************************************************************************************"
+
+echo "list files in APPCENTER_OUTPUT_DIRECTORY"
+ls $APPCENTER_OUTPUT_DIRECTORY
+
+##################################################
+# Start UI Tests
+##################################################
+
+# variables
+appCenterLoginApiToken=$AppCenterLoginForAutomatedUITests # this comes from the build environment variables
+appName="Trine-App/Trine-Android"
+deviceSetName="b82043e1"
+testSeriesName="ui-tests"
+
+echo ""
+echo "Start Xamarin.UITest run"
+echo "   App Name: $appName"
+echo " Device Set: $deviceSetName"
+echo "Test Series: $testSeriesName"
+echo ""
+
+echo "> Run UI test command"
+# Note: must put a space after each parameter/value pair
+appcenter test run uitest --app $appName --devices $deviceSetName --app-path $APPCENTER_OUTPUT_DIRECTORY/io.trine.trineapp-Signed.apk --test-series $testSeriesName --locale "fr_FR" --build-dir $APPCENTER_SOURCE_DIRECTORY/src/modules/Modules.Authentication.UITests/bin/Release --uitest-tools-dir $APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.*/tools --token $appCenterLoginApiToken 
+
+echo ""
+echo "**************************************************************************************************"
+echo "Post Build Script complete"
+echo "**************************************************************************************************"
