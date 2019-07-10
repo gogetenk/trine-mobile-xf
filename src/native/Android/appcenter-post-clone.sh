998f75a7080a104b20e0ebb17d93192d713b244a
Yannis <gogetenk@hotmail.com>
2019-07-11 00:56:06 +0200

    fvdfdvdfv

--- src/native/Android/appcenter-post-clone.sh
+++ src/native/Android/appcenter-post-clone.sh
@@ -1,4 +1,5 @@
 #!usr/bin/env bash -e
+    echo "**** Post Clone Script Started ***"
 
 echo "Found Unit Test projects:"
 find $APPCENTER_SOURCE_DIRECTORY -regex '.*UnitTests.*\.csproj' -exec echo {} \;
