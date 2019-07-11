9360876a28b52f0a37fb34d7b35f11a9ef6774a4
= <=>
2019-07-11 09:52:36 +0200

    ezzerze

--- src/native/Android/appcenter-post-clone.sh
+++ src/native/Android/appcenter-post-clone.sh
@@ -5,4 +5,5 @@ echo "Found Unit Test projects:"
 find $APPCENTER_SOURCE_DIRECTORY -regex '.*UnitTests.*\.csproj' -exec echo {} \;
 echo
 echo "Run Unit Test projects"
-find $APPCENTER_SOURCE_DIRECTORY -regex '.*UnitTests.*\.csproj' -exec xargs dotnet test;
+find $APPCENTER_SOURCE_DIRECTORY -regex '.*UnitTests.*\.csproj' | xargs dotnet test;
+
