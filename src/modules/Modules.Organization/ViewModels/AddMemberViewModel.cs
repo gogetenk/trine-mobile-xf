21414a930ca9409e43105e7e16c1e633a107bc64
Yannis <gogetenk@hotmail.com>
2019-07-15 23:38:57 +0200

    add member UI

---
+++ src/modules/Modules.Organization/ViewModels/AddMemberViewModel.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Prism.Logging;
+using Prism.Navigation;
+using Prism.Services;
+using Trine.Mobile.Components.ViewModels;
+
+namespace Modules.Organization.ViewModels
+{
+    public class AddMemberViewModel : ViewModelBase
+    {
+        public AddMemberViewModel(INavigationService navigationService, IMapper mapper, ILogger logger, IPageDialogService dialogService) : base(navigationService, mapper, logger, dialogService)
+        {
+        }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+        }
+    }
+}
--- src/modules/Modules.Organization/ViewModels/MembersViewModel.cs
+++ src/modules/Modules.Organization/ViewModels/MembersViewModel.cs
@@ -1,4 +1,7 @@
-﻿using AutoMapper;
+﻿using System.Threading.Tasks;
+using System.Windows.Input;
+using AutoMapper;
+using Prism.Commands;
 using Prism.Logging;
 using Prism.Navigation;
 using Prism.Services;
@@ -8,11 +11,18 @@ namespace Modules.Organization.ViewModels
 {
     public class MembersViewModel : ViewModelBase
     {
-        public string Title { get; set; } = "Panda Services";
+        public string OrganizationName { get; set; } = "Panda Services";
+        public string PageTitle { get; set; } = "Membres";
+        public ICommand AddMemberCommand { get; set; }
 
         public MembersViewModel(INavigationService navigationService, IMapper mapper, ILogger logger, IPageDialogService dialogService) : base(navigationService, mapper, logger, dialogService)
         {
+            AddMemberCommand = new DelegateCommand(async () => await OnAddMember());
+        }
 
+        private async Task OnAddMember()
+        {
+            await NavigationService.NavigateAsync("AddMemberView");
         }
     }
 }
