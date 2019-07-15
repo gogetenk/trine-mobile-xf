4eda4bc14eb2e51ddfcc99e8228a60f5ca9d54ca
Yannis <gogetenk@hotmail.com>
2019-07-15 22:53:48 +0200

    added entry custom rendereres

---
+++ src/native/iOS/Renderers/MaterialEntryRenderer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CoreAnimation;
+using CoreGraphics;
+using Foundation;
+using Trine.Mobile.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer (typeof(Entry), typeof(MaterialEntryRenderer))]
+namespace Trine.Mobile.iOS.Renderers
+{
+    public class MaterialEntryRenderer : EntryRenderer
+    {
+        private CALayer _line;
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            _line = null;
+
+            if (Control == null || e.NewElement == null)
+                return;
+
+            Control.BorderStyle = UITextBorderStyle.None;
+            Control.Layer.AddSublayer(_line);
+        }
+    }
+}
