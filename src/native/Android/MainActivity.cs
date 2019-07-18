fe640ddccad84971cc7d5e0c5055cab8ab7397e8
= <=>
2019-07-18 17:24:59 +0200

    added circleimage lib

--- src/native/Android/MainActivity.cs
+++ src/native/Android/MainActivity.cs
@@ -2,6 +2,7 @@
 using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
+using ImageCircle.Forms.Plugin.Droid;
 using Plugin.CurrentActivity;
 using Prism;
 using Prism.Ioc;
@@ -27,6 +28,7 @@ namespace Trine.Mobile.Android
             global::Xamarin.Forms.Forms.Init(this, bundle);
 
             LoadApplication(new App(new AndroidInitializer()));
+            ImageCircleRenderer.Init();
             FormsMaterial.Init(this, bundle);
             CrossCurrentActivity.Current.Init(this, bundle);
         }
