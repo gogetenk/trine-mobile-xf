aaf108eff23e48ec79ffaf4527013e892b6eea2a
Yannis <gogetenk@hotmail.com>
2019-07-30 23:22:05 +0200

    wip

---
+++ src/modules/Modules.Mission/ViewModels/CreateMissionContractViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modules.Mission.ViewModels
+{
+    class CreateMissionContractViewModel
+    {
+    }
+}
--- src/modules/Modules.Mission/ViewModels/CreateMissionPriceViewModel.cs
+++ src/modules/Modules.Mission/ViewModels/CreateMissionPriceViewModel.cs
@@ -1,7 +1,12 @@
-﻿using AutoMapper;
+﻿using System;
+using System.Threading.Tasks;
+using AutoMapper;
+using Prism.Commands;
 using Prism.Logging;
 using Prism.Navigation;
 using Prism.Services;
+using Trine.Mobile.Components.Navigation;
+using Trine.Mobile.Dto;
 
 namespace Modules.Mission.ViewModels
 {
@@ -9,11 +14,38 @@ namespace Modules.Mission.ViewModels
     {
         #region Bindings
 
+        public bool IsDailyRateErrorVisible { get; set; } = false;
 
         #endregion
 
         public CreateMissionPriceViewModel(INavigationService navigationService, IMapper mapper, ILogger logger, IPageDialogService dialogService) : base(navigationService, mapper, logger, dialogService)
         {
+            NextCommand = new DelegateCommand(async () => await OnNextStep());
+        }
+
+        public override void OnNavigatedTo(INavigationParameters parameters)
+        {
+            base.OnNavigatedTo(parameters);
+
+            if (CreateMissionRequest.DailyPrice == 0)
+                CreateMissionRequest.DailyPrice = 400;
+
+            if (CreateMissionRequest.CommercialFeePercentage == 0 && CreateMissionRequest.IsTripartite)
+                CreateMissionRequest.CommercialFeePercentage = 15;
+        }
+
+        private async Task OnNextStep()
+        {
+            IsUserErrorVisible = PickedUser is null;
+            if (IsUserErrorVisible)
+                return;
+
+            var createMissionRequest = new CreateMissionRequestDto();
+            createMissionRequest.Commercial = PickedUser;
+
+            var navigationParams = new NavigationParameters();
+            navigationParams.Add(NavigationParameterKeys._CreateMissionRequest, createMissionRequest);
+            await NavigationService.NavigateAsync("CreateMissionContractView", navigationParams, useModalNavigation: false);
         }
     }
 }
