7b70f1e4768164505e89db2c6ceeb20afa0cad34
Yannis <gogetenk@hotmail.com>
2019-08-03 16:56:45 +0200

    added error screen

--- src/modules/Modules.Mission/ViewModels/CreateMissionSuccessViewModel.cs
+++ src/modules/Modules.Mission/ViewModels/CreateMissionSuccessViewModel.cs
@@ -1,12 +1,12 @@
-﻿using AutoMapper;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using AutoMapper;
 using Prism.Commands;
 using Prism.Logging;
 using Prism.Navigation;
 using Prism.Services;
 using Sogetrel.Sinapse.Framework.Exceptions;
-using System;
-using System.Threading.Tasks;
-using System.Windows.Input;
 using Trine.Mobile.Bll;
 using Trine.Mobile.Bll.Impl.Messages;
 using Trine.Mobile.Dto;
@@ -16,13 +16,21 @@ namespace Modules.Mission.ViewModels
 {
     public class CreateMissionSuccessViewModel : CreateMissionViewModelBase
     {
-        #region Bindings
+        private readonly IMissionService _missionService;
+
+        #region Bindings 
 
         public ICommand NavigateToMissionDetailsCommand { get; set; }
-        private readonly IMissionService _missionService;
-        
+        public ICommand RetryCommand { get; set; }
+
         private bool _isLoading = true;
         public bool IsLoading { get => _isLoading; set { _isLoading = value; RaisePropertyChanged(); } }
+
+        private bool _isSuccessShown = false;
+        public bool IsSuccessShown { get => _isSuccessShown; set { _isSuccessShown = value; RaisePropertyChanged(); } }
+
+        private bool _isErrorShown = false;
+        public bool IsErrorShown { get => _isErrorShown; set { _isErrorShown = value; RaisePropertyChanged(); } }
 
         private bool _isGridEnabled = false;
         public bool IsGridEnabled { get => _isGridEnabled; set { _isGridEnabled = value; RaisePropertyChanged(); } }
@@ -35,6 +43,8 @@ namespace Modules.Mission.ViewModels
         public CreateMissionSuccessViewModel(INavigationService navigationService, IMapper mapper, ILogger logger, IPageDialogService dialogService, IMissionService missionService) : base(navigationService, mapper, logger, dialogService)
         {
             NavigateToMissionDetailsCommand = new DelegateCommand(async () => await NavigateToMissionDetails());
+            RetryCommand = new DelegateCommand(async () => await OnRetry());
+
             _missionService = missionService;
         }
 
@@ -42,6 +52,11 @@ namespace Modules.Mission.ViewModels
         {
             base.OnNavigatedTo(parameters);
 
+            await CreateMission();
+        }
+
+        private async Task OnRetry()
+        {
             await CreateMission();
         }
 
@@ -55,14 +70,20 @@ namespace Modules.Mission.ViewModels
                 if (CreatedMission is null)
                     throw new BusinessException(ErrorMessages.unknownError);
 
+                IsErrorShown = false;
+                IsSuccessShown = true;
                 IsGridEnabled = true;
             }
             catch (BusinessException bExc)
             {
+                IsErrorShown = false;
+                IsSuccessShown = true;
                 await LogAndShowBusinessError(bExc);
             }
             catch (Exception exc)
             {
+                IsErrorShown = false;
+                IsSuccessShown = true;
                 LogTechnicalError(exc);
             }
             finally
