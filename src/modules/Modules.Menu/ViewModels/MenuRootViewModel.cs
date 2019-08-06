f8148095bc18ba2092c70a946228780f2a792421
Yannis <gogetenk@hotmail.com>
2019-08-06 23:47:15 +0200

    activated join organization from the menu

--- src/modules/Modules.Menu/ViewModels/MenuRootViewModel.cs
+++ src/modules/Modules.Menu/ViewModels/MenuRootViewModel.cs
@@ -12,15 +12,22 @@ namespace Modules.Menu.ViewModels
     public class MenuRootViewModel : ViewModelBase
     {
         public ICommand CreateMissionCommand { get; set; }
+        public ICommand CreateOrganizationCommand { get; set; }
 
         public MenuRootViewModel(INavigationService navigationService, IMapper mapper, ILogger logger, IPageDialogService dialogService) : base(navigationService, mapper, logger, dialogService)
         {
             CreateMissionCommand = new DelegateCommand(async () => await OnCreateMission());
+            CreateOrganizationCommand = new DelegateCommand(async () => await OnAddOrganization());
         }
 
         private async Task OnCreateMission()
         {
             await NavigationService.NavigateAsync("TrineNavigationPage/CreateMissionStartView", useModalNavigation: true);
+        }
+
+        private async Task OnAddOrganization()
+        {
+            await NavigationService.NavigateAsync("OrganizationChoiceView", useModalNavigation: true);
         }
     }
 }
