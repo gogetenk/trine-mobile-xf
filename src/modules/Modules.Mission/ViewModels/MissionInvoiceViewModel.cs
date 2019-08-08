f1cf06c5d68ecb7ad216ad0c413b5e8dbfb2b154
Yannis <gogetenk@hotmail.com>
2019-08-08 23:15:58 +0200

    created all mission details pages

---
+++ src/modules/Modules.Mission/ViewModels/MissionInvoiceViewModel.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using Prism;
+using Prism.Logging;
+using Prism.Navigation;
+using Prism.Services;
+using System;
+using Trine.Mobile.Components.ViewModels;
+
+namespace Modules.Mission.ViewModels
+{
+    public class MissionInvoiceViewModel : ViewModelBase, IActiveAware
+    {
+        public event EventHandler IsActiveChanged;
+        private bool _isActive;
+        public bool IsActive
+        {
+            get { return _isActive; }
+            set { SetProperty(ref _isActive, value, RaiseIsActiveChanged); }
+        }
+
+        public MissionInvoiceViewModel(INavigationService navigationService, IMapper mapper, ILogger logger, IPageDialogService dialogService) : base(navigationService, mapper, logger, dialogService)
+        {
+        }
+
+        // Triggered only on tabbed pages
+        protected virtual async void RaiseIsActiveChanged()
+        {
+            IsActiveChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
