938bd6016016c6863566bf4961fef54a6f72f902
Yannis <gogetenk@hotmail.com>
2019-08-08 21:18:05 +0200

    plugged api

--- src/modules/Modules.Menu/ViewModels/MenuViewModel.cs
+++ src/modules/Modules.Menu/ViewModels/MenuViewModel.cs
@@ -8,7 +8,6 @@ namespace Modules.Menu.ViewModels
 {
     public class MenuViewModel : ViewModelBase
     {
-
         public MenuViewModel(INavigationService navigationService, IMapper mapper, ILogger logger, IPageDialogService dialogService) : base(navigationService, mapper, logger, dialogService)
         {
         }
--- src/modules/Modules.Organization/ViewModels/OrganizationHomeViewModel.cs
+++ src/modules/Modules.Organization/ViewModels/OrganizationHomeViewModel.cs
@@ -4,7 +4,9 @@ using Prism.Logging;
 using Prism.Navigation;
 using Prism.Services;
 using System;
+using Trine.Mobile.Components.Navigation;
 using Trine.Mobile.Components.ViewModels;
+using Trine.Mobile.Dto;
 
 namespace Modules.Organization.ViewModels
 {
@@ -18,13 +20,20 @@ namespace Modules.Organization.ViewModels
             set { SetProperty(ref _isActive, value, RaiseIsActiveChanged); }
         }
 
+        private PartialOrganizationDto _organization;
+        public PartialOrganizationDto Organization { get => _organization; set { _organization = value; RaisePropertyChanged(); } }
+
         public OrganizationHomeViewModel(INavigationService navigationService, IMapper mapper, ILogger logger, IPageDialogService dialogService) : base(navigationService, mapper, logger, dialogService)
         {
         }
 
-        public override void OnNavigatedTo(INavigationParameters parameters)
+        public override async void OnNavigatedTo(INavigationParameters parameters)
         {
             base.OnNavigatedTo(parameters);
+
+            Organization = parameters.GetValue<PartialOrganizationDto>(NavigationParameterKeys._Organization);
+            if (Organization is null)
+                await NavigationService.GoBackAsync();
         }
 
         // Triggered only on tabbed pages
