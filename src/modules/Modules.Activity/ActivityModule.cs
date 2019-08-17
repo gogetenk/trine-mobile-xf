2fed907f630696cc78d5cb10f645285b305402ff
Yannis <gogetenk@hotmail.com>
2019-08-18 01:18:41 +0200

    init module

---
+++ src/modules/Modules.Activity/ActivityModule.cs
@@ -0,0 +1,20 @@
+﻿using Modules.Activity.ViewModels;
+using Modules.Activity.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+
+namespace Modules.Activity
+{
+    public class ActivityModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<ActivityDetailsView, ActivityDetailsViewModel>();
+        }
+    }
+}
---
+++ src/modules/Modules.Activity/ViewModels/ActivityDetailsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Prism.Logging;
+using Prism.Navigation;
+using Prism.Services;
+using Trine.Mobile.Components.ViewModels;
+
+namespace Modules.Activity.ViewModels
+{
+    public class ActivityDetailsViewModel : ViewModelBase
+    {
+        public ActivityDetailsViewModel(INavigationService navigationService, IMapper mapper, ILogger logger, IPageDialogService dialogService) : base(navigationService, mapper, logger, dialogService)
+        {
+        }
+    }
+}
