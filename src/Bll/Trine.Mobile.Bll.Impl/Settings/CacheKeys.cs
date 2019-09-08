9a07dd4e42b2528486b7c28ae4ec8fa1b8d206ea
Yannis <gogetenk@hotmail.com>
2019-09-08 11:03:51 +0200

    Cached user so we dont need to login multiple times

---
+++ src/Bll/Trine.Mobile.Bll.Impl/Settings/CacheKeys.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trine.Mobile.Bll.Impl.Settings
+{
+    public class CacheKeys
+    {
+        public static string _CurrentUser = "CurrentUser";
+    }
+}
