a63bd1ced368374f92e38d19b81e3b7df8403dd5
Yannis <gogetenk@hotmail.com>
2019-09-23 20:48:38 +0200

    enabled back hotreload

--- src/Bootstrapper/App.xaml.cs
+++ src/Bootstrapper/App.xaml.cs
@@ -30,6 +30,7 @@ using Trine.Mobile.Dal.AzureBlobStorage.Repositories;
 using Trine.Mobile.Dal.Configuration;
 using Trine.Mobile.Dal.Swagger;
 using Xamarin.Essentials;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
@@ -62,7 +63,7 @@ namespace Trine.Mobile.Bootstrapper
                 //    .StartInit("12785512-a98b-4c91-89ca-05959a685120")
                 //    .EndInit();
 #if DEBUG
-                //HotReloader.Current.Run(this);
+                HotReloader.Current.Run(this);
 #endif
                 //Akavache.Registrations.Start("TrineApp");
                 await NavigationService.NavigateAsync("TrineNavigationPage/SignupView");
