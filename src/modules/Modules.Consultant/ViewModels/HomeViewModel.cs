69863b0420906224217a34f15206405bd1850eab
yannis <yannis.tocreau@sogetrel.fr>
2019-11-07 15:29:41 +0100

    almost finished UI

--- src/modules/Modules.Consultant/ViewModels/HomeViewModel.cs
+++ src/modules/Modules.Consultant/ViewModels/HomeViewModel.cs
@@ -1,13 +1,23 @@
-﻿using Prism.Navigation;
+﻿using AutoFixture;
+using Prism.Navigation;
 using Prism.Services;
 using Trine.Mobile.Components.ViewModels;
+using Trine.Mobile.Dto;
 
 namespace Modules.Consultant.ViewModels
 {
     public class HomeViewModel : ViewModelBase
     {
+        #region Bindings
+
+        private ActivityDto _activity;
+        public ActivityDto Activity { get => _activity; set { _activity = value; RaisePropertyChanged(); } }
+
+        #endregion
+
         public HomeViewModel(INavigationService navigationService, global::AutoMapper.IMapper mapper, Prism.Logging.ILogger logger, IPageDialogService dialogService) : base(navigationService, mapper, logger, dialogService)
         {
+            Activity = new Fixture().Create<ActivityDto>();
         }
     }
 }
