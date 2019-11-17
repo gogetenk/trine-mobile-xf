4c31a0c15bdbfd205abb18db093bf11c3a13b061
Yannis <gogetenk@hotmail.com>
2019-11-17 19:21:36 +0100

    added new button logic

---
+++ src/Components/Converters/IsActivityCompletedConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Trine.Mobile.Dto;
+using Xamarin.Forms;
+
+namespace Trine.Mobile.Components.Converters
+{
+    public class IsActivityCompletedConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Can consultant modify ?
+            return (((ActivityStatusEnum)value) == ActivityStatusEnum.CustomerSigned);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
