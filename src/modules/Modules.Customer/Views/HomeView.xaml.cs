fe329324386654d10c1f830cd01a947989b705db
yannis <yannis.tocreau@sogetrel.fr>
2019-11-18 18:41:58 +0100

    ios UI fixes

--- src/modules/Modules.Customer/Views/HomeView.xaml.cs
+++ src/modules/Modules.Customer/Views/HomeView.xaml.cs
@@ -12,10 +12,14 @@ namespace Modules.Customer.Views
             InitializeComponent();
         }
 
+
+
         private void ScrollView_Scrolled(object sender, ScrolledEventArgs e)
         {
+#if __ANDROID__
             grid_header.TranslationY = -e.ScrollY / 2.5;
             grid_header_title.TranslationY = -e.ScrollY / 4;
+#endif
 
             //grid_header.Opacity = Normalize(e.ScrollY, 0, 500, 1, 0);
         }
