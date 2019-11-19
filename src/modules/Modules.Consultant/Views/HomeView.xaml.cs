557e3938fc1048c566d7f97f590d75cbe3bb980d
Yannis <gogetenk@hotmail.com>
2019-11-19 10:43:09 +0100

    added empty state for customer

--- src/modules/Modules.Consultant/Views/HomeView.xaml.cs
+++ src/modules/Modules.Consultant/Views/HomeView.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Modules.Consultant.Views
@@ -15,6 +9,8 @@ namespace Modules.Consultant.Views
         public HomeView()
         {
             InitializeComponent();
+
+            frame_comment.IsVisible = false;
         }
     }
 }
