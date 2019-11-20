88948b92c2a334ee3840ef52197084c8b96ae969
Yannis <gogetenk@hotmail.com>
2019-11-20 10:35:11 +0100

    custom shadows with elevation on android and ios

---
+++ src/Components/Controls/ElevationFrame.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms;
+
+namespace Trine.Mobile.Components.Controls
+{
+    public class ElevationFrame : Frame
+    {
+        public static BindableProperty ElevationProperty = BindableProperty.Create(nameof(Elevation), typeof(float), typeof(ElevationFrame), 4.0f);
+        public float Elevation
+        {
+            get
+            {
+                return (float)GetValue(ElevationProperty);
+            }
+            set
+            {
+                SetValue(ElevationProperty, value);
+            }
+        }
+    }
+}
