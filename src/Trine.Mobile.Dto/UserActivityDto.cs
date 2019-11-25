a2ab4896cff3667847f87528a6fed7d43772cdfb
Yannis <gogetenk@hotmail.com>
2019-11-25 20:33:06 +0100

    bug fix mapping

--- src/Trine.Mobile.Dto/UserActivityDto.cs
+++ src/Trine.Mobile.Dto/UserActivityDto.cs
@@ -7,7 +7,7 @@ namespace Trine.Mobile.Dto
         public string Id { get; set; }
         public string Firstname { get; set; }
         public string Lastname { get; set; }
-        public string FullName { get; set; }
+        public string Fullname { get; set; }
         public string ProfilePicUrl { get; set; }
         public DateTime? SignatureDate { get; set; }
         public string SignatureUri { get; set; }
