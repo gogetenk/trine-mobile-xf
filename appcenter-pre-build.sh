fcb34082aed185860e3fc1708c84acc35ad23545
Yannis <gogetenk@hotmail.com>
2019-11-25 20:04:39 +0100

    fixed bash scripts

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -3,8 +3,8 @@
 # iOS SPECIFIC, DON'T USE THAT FOR ANDROID BUILDS
 ###
 
-if [ "$APPCENTER_BRANCH" == "master" ] || [ "$APPCENTER_BRANCH" == "release" ];
-  sed -i .bak 's/app-assistance-dev.azurewebsites.net/app-assistance.azurewebsites.net/g' src/Bll/Trine.Mobile.Bll.Impl/Settings/AppSettings.cs
+if [ $APPCENTER_BRANCH = "master" ] || [ $APPCENTER_BRANCH = "release" ]
 then
-  
+	echo "Replacing connection strings."
+	sed -i .bak 's/app-assistance-dev.azurewebsites.net/app-assistance.azurewebsites.net/g' src/Bll/Trine.Mobile.Bll.Impl/Settings/AppSettings.cs
 fi
