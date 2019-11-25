3a280ceb0be5cc6c516f50ca8268ffb0785288a6
yannis <yannis.tocreau@sogetrel.fr>
2019-11-25 17:47:11 +0100

    bug fix

--- src/Model/Trine.Mobile.Model/UserActivityModel.cs
+++ src/Model/Trine.Mobile.Model/UserActivityModel.cs
@@ -7,7 +7,9 @@ namespace Trine.Mobile.Model
         public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string FullName { get; set; }
         public string ProfilePicUrl { get; set; }
         public DateTime? SignatureDate { get; set; }
+        public string SignatureUri { get; set; }
     }
 }
