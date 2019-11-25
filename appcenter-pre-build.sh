f91d1ae6bb4362ee5eba7bdbaba11aaa6a3e4309
Yannis <gogetenk@hotmail.com>
2019-11-25 23:09:28 +0100

    fix scripts

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -3,8 +3,3 @@
 # iOS SPECIFIC, DON'T USE THAT FOR ANDROID BUILDS
 ###
 
-if [ $APPCENTER_BRANCH = "master" ] || [ $APPCENTER_BRANCH = "release" ]
-then
-	#echo "Replacing connection strings."
-	#sed -i .bak 's/app-assistance-dev.azurewebsites.net/app-assistance.azurewebsites.net/g' src/Bll/Trine.Mobile.Bll.Impl/Settings/AppSettings.cs
-fi
