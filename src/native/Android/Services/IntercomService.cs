80d39a03929c29100fea8657de63bee29cf1cf8a
yannis <yannis.tocreau@sogetrel.fr>
2019-11-26 17:05:45 +0100

    updated googlejson + disabled intercom

--- src/native/Android/Services/IntercomService.cs
+++ src/native/Android/Services/IntercomService.cs
@@ -1,7 +1,4 @@
-﻿using Binding.Intercom.Android;
-using Binding.Intercom.Android.Identity;
-using Java.Lang;
-using Trine.Mobile.Bll;
+﻿using Trine.Mobile.Bll;
 using Trine.Mobile.Model;
 
 namespace Trine.Mobile.Bootstrapper.Droid.Services
@@ -10,56 +7,56 @@ namespace Trine.Mobile.Bootstrapper.Droid.Services
     {
         public void RegisterAnonymousUser()
         {
-            Intercom.Client().RegisterUnidentifiedUser();
+            //Intercom.Client().RegisterUnidentifiedUser();
         }
 
         public void RegisterUser(UserModel user)
         {
-            Registration registration;
+            //Registration registration;
 
-            if (user == null)
-            {
-                Intercom.Client().RegisterUnidentifiedUser();
-                return;
-            }
+            //if (user == null)
+            //{
+            //    Intercom.Client().RegisterUnidentifiedUser();
+            //    return;
+            //}
 
-            var currentUser = user;
-            Company company = null;
+            //var currentUser = user;
+            //Company company = null;
 
-            if (currentUser.Company != null)
-                company = new Company.Builder()
-                    .WithName(currentUser.Company.Name)
-                    .WithCompanyId(currentUser.Company.Id)
-                    .Build();
+            //if (currentUser.Company != null)
+            //    company = new Company.Builder()
+            //        .WithName(currentUser.Company.Name)
+            //        .WithCompanyId(currentUser.Company.Id)
+            //        .Build();
 
-            var attributes = new UserAttributes.Builder()
-                .WithName(currentUser.Firstname)
-                .WithEmail(currentUser.Mail)
-                .WithUserId(currentUser.Id)
-                .WithPhone(currentUser.PhoneNumber)
-                .WithSignedUpAt((Long)Long.ParseLong(currentUser.SubscriptionDate.Ticks.ToString()))
-                .WithCompany(company)
-                .Build();
+            //var attributes = new UserAttributes.Builder()
+            //    .WithName(currentUser.Firstname)
+            //    .WithEmail(currentUser.Mail)
+            //    .WithUserId(currentUser.Id)
+            //    .WithPhone(currentUser.PhoneNumber)
+            //    .WithSignedUpAt((Long)Long.ParseLong(currentUser.SubscriptionDate.Ticks.ToString()))
+            //    .WithCompany(company)
+            //    .Build();
 
-            registration = Registration.Create().WithUserId(currentUser.Id).WithUserAttributes(attributes);
-            Intercom.Client().RegisterIdentifiedUser(registration);
+            //registration = Registration.Create().WithUserId(currentUser.Id).WithUserAttributes(attributes);
+            //Intercom.Client().RegisterIdentifiedUser(registration);
 
-            Intercom.Client().HandlePushMessage();
+            //Intercom.Client().HandlePushMessage();
         }
 
         public void ShowHelpCenter()
         {
-            Intercom.Client().DisplayHelpCenter();
+            //Intercom.Client().DisplayHelpCenter();
         }
 
         public void ShowMessenger()
         {
-            Intercom.Client().DisplayMessenger();
+            //Intercom.Client().DisplayMessenger();
         }
 
         public void Logout()
         {
-            Intercom.Client().Logout();
+            //Intercom.Client().Logout();
         }
     }
 }
