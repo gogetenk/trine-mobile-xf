15266d3bf744d0bd84b90f3ccf67e038ffe7e0b6
Rémi Roycourt <remiroycourt@gmail.com>
2019-11-27 09:55:08 +0100

    build: test pre-build script

--- src/native/Android/appcenter-pre-build.sh
+++ src/native/Android/appcenter-pre-build.sh
@@ -3,3 +3,9 @@
 # ANDROID SPECIFIC, DON'T USE THAT FOR iOS BUILDS
 ###
 
+if [ "$APPCENTER_BRANCH" == "master" ];
+  sed -i .bak 's/app-assistance-dev.azurewebsites.net/app-assistance.azurewebsites.net/g' ../../Bll/Trine.Mobile.Bll.Impl/Settings/AppSettings.cs
+  cat ../../Bll/Trine.Mobile.Bll.Impl/Settings/AppSettings.cs
+then
+
+fi
