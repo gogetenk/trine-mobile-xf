b781c53cf0c38203adfc568d09eaf6cfb5b87923
= <=>
2019-12-01 23:51:33 +0100

    fixes

--- src/modules/Modules.Consultant/ViewModels/HomeViewModel.cs
+++ src/modules/Modules.Consultant/ViewModels/HomeViewModel.cs
@@ -332,7 +332,7 @@ namespace Modules.Consultant.ViewModels
 
         private async Task OnHistoryTapped()
         {
-            await NavigationService.NavigateAsync("../TrineNavigationPage/ActivityHistoryView");
+            await NavigationService.NavigateAsync("TrineNavigationPage/ActivityHistoryView");
         }
     }
 }
