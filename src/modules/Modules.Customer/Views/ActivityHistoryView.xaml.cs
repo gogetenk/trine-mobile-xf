8af0e9e816ce69e4888958c7dac787bf531c8ed4
Rémi Roycourt <remiroycourt@gmail.com>
2019-12-04 00:27:37 +0000

    Merged PR 102: Activity changes

--- src/modules/Modules.Customer/Views/ActivityHistoryView.xaml.cs
+++ src/modules/Modules.Customer/Views/ActivityHistoryView.xaml.cs
@@ -11,20 +11,5 @@ namespace Modules.Customer.Views
         {
             InitializeComponent();
         }
-
-        private void ScrollView_Scrolled(object sender, ScrolledEventArgs e)
-        {
-#if __ANDROID__
-            grid_header.TranslationY = -e.ScrollY / 2.5;
-            grid_header_title.TranslationY = -e.ScrollY / 4;
-#endif
-
-            //grid_header.Opacity = Normalize(e.ScrollY, 0, 500, 1, 0);
-        }
-
-        private double Normalize(double val, double valmin, double valmax, double min, double max)
-        {
-            return (((val - valmin) / (valmax - valmin)) * (max - min)) + min;
-        }
     }
 }
